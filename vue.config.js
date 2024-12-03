dc70dd704e93f826fe829378cd42a10e25966feb
Андрей кропинов <andreikropinov17121988@gmail.com>
2024-12-03 22:31:13 +0300

    edit

--- vue.config.js
+++ vue.config.js
@@ -2,29 +2,26 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: '/Threejs/', // Устанавливаем базовый путь принудительно
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === 'production' ? '/Threejs/' : '/',
   build: {
-    outDir: 'dist',  // Папка для сборки
-    assetsDir: 'assets',  // Папка для статичных ресурсов
+    outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       output: {
-        assetFileNames: 'assets/[name].[hash].[ext]',  // Генерация файлов с хешами для предотвращения кэширования
-        chunkFileNames: 'assets/[name].[hash].js',  // Путь к JS-чанкам
-        entryFileNames: 'assets/[name].js',  // Путь к основным JS-файлам
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].js',
       },
     },
   },
   css: {
-    extract: true,  // извлечение CSS в отдельный файл
+    extract: true,
   },
-})
+});
